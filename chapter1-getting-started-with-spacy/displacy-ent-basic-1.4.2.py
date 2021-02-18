0e637edc057d41bb989e79d268306c66930f3fdc
Kevin Lu <kevinlu1248@gmail.com>
2021-02-17 20:37:22 -0800

    Update displacy-ent-basic-1.4.2.py
    Changed `server` to `serve`

--- chapter1-getting-started-with-spacy/displacy-ent-basic-1.4.2.py
+++ chapter1-getting-started-with-spacy/displacy-ent-basic-1.4.2.py
@@ -4,5 +4,5 @@ import spacy
 from spacy import displacy
 nlp = spacy.load("en")
 doc = nlp("Bill Gates is the CEO of Microsoft.")
-displacy.server(doc, style="ent")
+displacy.serve(doc, style="ent")
 
