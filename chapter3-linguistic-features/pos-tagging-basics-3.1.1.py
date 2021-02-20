8dcb77a699f9b8e739ecb92ec8296e6a6f265c6d
Kevin Lu <kevinlu1248@gmail.com>
2021-02-19 23:38:59 -0800

    Update pos-tagging-basics-3.1.1.py

--- chapter3-linguistic-features/pos-tagging-basics-3.1.1.py
+++ chapter3-linguistic-features/pos-tagging-basics-3.1.1.py
@@ -5,5 +5,5 @@ nlp = spacy.load("en")
 
 doc = nlp("Alicia and me went to the school by bus")
 for token in doc:
-    print(token.text, otken.pos_, token.tag), spacy.explain(token.pos_), spacy.explain(token.tag_))
+    print(token.text, token.pos_, token.tag, spacy.explain(token.pos_), spacy.explain(token.tag_))
 
