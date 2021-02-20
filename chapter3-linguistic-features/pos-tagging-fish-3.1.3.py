056bd2864304628597d9daa4d920314d910b13db
Kevin Lu <kevinlu1248@gmail.com>
2021-02-19 23:39:56 -0800

    Update pos-tagging-fish-3.1.3.py

--- chapter3-linguistic-features/pos-tagging-fish-3.1.3.py
+++ chapter3-linguistic-features/pos-tagging-fish-3.1.3.py
@@ -5,5 +5,4 @@ nlp = spacy.load("en")
 
 doc = nlp("My cat will fish for a fish tomorrow in a fishy way.")
 for token in doc:
-    print(token.text, otken.pos_, token.tag), spacy.explain(token.pos_), spacy.explain(token.tag_))
-
+    print(token.text, token.pos_, token.tag, spacy.explain(token.pos_), spacy.explain(token.tag_))
