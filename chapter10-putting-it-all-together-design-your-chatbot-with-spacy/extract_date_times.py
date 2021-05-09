a66795e674a8fcde217f0fbbe896369e40faa3dc
Duygu <duygu.altinok12@gmail.com>
2021-05-09 21:25:56 +0200

    added code and data

---
+++ chapter10-putting-it-all-together-design-your-chatbot-with-spacy/extract_date_times.py
@@ -0,0 +1,14 @@
+import spacy
+
+nlp = spacy.load("en_core_web_md")
+
+
+with open("data/utterances.txt", "r") as utterances:
+    for utterance in utterances:
+        utterance = utterance.strip()
+        doc = nlp(utterance)
+        ents = doc.ents
+        if ents:
+            for ent in ents:
+                if ent.label_ == "TIME" or ent.label_ ==  "DATE":
+                    print(ent.text, "\t", utterance)
